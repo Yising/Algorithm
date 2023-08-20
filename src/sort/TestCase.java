1f2d3b3ee40b22a7aba25106a38f5e035222a878
Yising <893746087@qq.com>
2023-08-20 14:04:49 +0800

    补充测试用例

--- src/sort/TestCase.java
+++ src/sort/TestCase.java
@@ -3,7 +3,8 @@ package sort;
 import java.util.List;
 
 public class TestCase {
-    private static final int[][] arrays = {{20, 40, 30, 10, 60, 50}};
+    private static final int[][] arrays = {{20, 40, 30, 10, 60, 50}, {5, 2, 3, 1}, {5, 1, 1, 2, 0, 0}};
+
     public static void main(String[] args) {
         for (int i = 0; i < arrays.length; i++) {
             sort(arrays[i]);
